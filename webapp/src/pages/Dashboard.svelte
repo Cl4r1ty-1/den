613bf4d1e9c9743493c1743348ede2bdb45cdae5
ella <ella@hack.ngo>
2025-09-12 17:10:27 +0000

    woof woof!

--- webapp/src/pages/Dashboard.svelte
+++ webapp/src/pages/Dashboard.svelte
@@ -4,7 +4,7 @@
 	import ToastContainer from '../lib/ToastContainer.svelte'
 	
 	export let user
-	export let container
+	export let container = null
 	export let subdomains = []
 
 	let showSubdomainModal = false
@@ -151,7 +151,7 @@
 								get new port
 							</button>
 						</div>
-						{#if container.AllocatedPorts && container.AllocatedPorts.length}
+						{#if container?.AllocatedPorts?.length}
 							<div class="flex flex-wrap gap-2">
 								{#each container.AllocatedPorts as port}
 									<div class="nb-pill">{port}</div>
@@ -197,7 +197,7 @@
 						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"></path>
 					</svg>
 				</div>
-				<div class="nb-title text-2xl">{subdomains.length}</div>
+				<div class="nb-title text-2xl">{subdomains?.length || 0}</div>
 				<div class="nb-text-muted text-sm">subdomains</div>
 			</div>
 			
@@ -226,7 +226,7 @@
 			{/if}
 		</div>
 		
-		{#if subdomains.length}
+		{#if subdomains?.length}
 			<div class="grid gap-4">
 				{#each subdomains as subdomain}
 					<div class="nb-card flex items-center justify-between">
